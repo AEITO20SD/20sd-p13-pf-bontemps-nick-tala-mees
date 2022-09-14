59cde6f2c10f6c4d9947b3a437753ccacbef591e
Meesie2109 <meesrootjes@icloud.com>
2022-09-14 23:44:30 +0200

    #42 Merging persoonlijke branch naar development banch (#43)
    * Merging acceptance in master (#16)
    
    Branch rules geskipt om wat verschillende dingen te testen met de branch rules
    
    * Acceptance (#30)
    
    * Uodated the readme
    
    * Changed the readme
    
    Changed the readme to include more information on the project. And include some important links like discord and google drive.
    
    * .gitignore aangepast
    
    .gitignore aangepast om hem alvast ook NodeJS en Angular te ondersteunen waar we mee gaan werken
    
    * #20 Merging development in testing (#21) (#28)
    
    * Iets kleins aangepast
    
    Iets kleins aangepast om een pull request te kunnen testen
    
    * #20 npm toegevoegd aan backend folder
    
    npm toegevoegd aan de backend folder, om zometeen NodeJS te kunnen installeren.
    
    * #20 Typescript toegevoegd aan de backend
    
    typescript toegevoegd aan de backend, om zo NodeJS mee te kunnen gaan runnen.
    
    * #20 Meerdere dev-dependencies geinstalleerd
    
    Meerdere dev-dependencies geinstalleerd, om zometeen NodeJS goed te kunnen laten draaien met Typescript.
    
    * #20 Installed NodeJS en Typescript voor de backend
    
    * #20 Standaard project via angular cli geinstalleerd
    
    * #24 merging acceptance in master (#35)
    
    * Uodated the readme
    
    * Changed the readme
    
    Changed the readme to include more information on the project. And include some important links like discord and google drive.
    
    * .gitignore aangepast
    
    .gitignore aangepast om hem alvast ook NodeJS en Angular te ondersteunen waar we mee gaan werken
    
    * #20 Merging development in testing (#21) (#28)
    
    * Iets kleins aangepast
    
    Iets kleins aangepast om een pull request te kunnen testen
    
    * #20 npm toegevoegd aan backend folder
    
    npm toegevoegd aan de backend folder, om zometeen NodeJS te kunnen installeren.
    
    * #20 Typescript toegevoegd aan de backend
    
    typescript toegevoegd aan de backend, om zo NodeJS mee te kunnen gaan runnen.
    
    * #20 Meerdere dev-dependencies geinstalleerd
    
    Meerdere dev-dependencies geinstalleerd, om zometeen NodeJS goed te kunnen laten draaien met Typescript.
    
    * #20 Installed NodeJS en Typescript voor de backend
    
    * #20 Standaard project via angular cli geinstalleerd
    
    * #24 Merging testing in acceptance (#34)
    
    * #20 Merging development in testing (#21)
    
    * Iets kleins aangepast
    
    Iets kleins aangepast om een pull request te kunnen testen
    
    * #20 npm toegevoegd aan backend folder
    
    npm toegevoegd aan de backend folder, om zometeen NodeJS te kunnen installeren.
    
    * #20 Typescript toegevoegd aan de backend
    
    typescript toegevoegd aan de backend, om zo NodeJS mee te kunnen gaan runnen.
    
    * #20 Meerdere dev-dependencies geinstalleerd
    
    Meerdere dev-dependencies geinstalleerd, om zometeen NodeJS goed te kunnen laten draaien met Typescript.
    
    * #20 Installed NodeJS en Typescript voor de backend
    
    * #20 Standaard project via angular cli geinstalleerd
    
    * Merging development in testing (#33)
    
    * Iets kleins aangepast
    
    Iets kleins aangepast om een pull request te kunnen testen
    
    * #20 npm toegevoegd aan backend folder
    
    npm toegevoegd aan de backend folder, om zometeen NodeJS te kunnen installeren.
    
    * #20 Typescript toegevoegd aan de backend
    
    typescript toegevoegd aan de backend, om zo NodeJS mee te kunnen gaan runnen.
    
    * #20 Meerdere dev-dependencies geinstalleerd
    
    Meerdere dev-dependencies geinstalleerd, om zometeen NodeJS goed te kunnen laten draaien met Typescript.
    
    * #20 Installed NodeJS en Typescript voor de backend
    
    * #20 Standaard project via angular cli geinstalleerd
    
    * #24 Onnodige informatie uit de angular app verwijderd
    
    * $42 standaard api files toegevoegd
    
    standaard api files toegevoegd aan de angular frontend om zometeen dat vanaf de backend te kunnen ontvangen
    
    * #42 connected de frontend en backend
    
    * #42 Response is nu ook zichtbaar in de frontend
    
    API response word nu ook geprint op het scherm in de frontend.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiserviceService } from './apiservice.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(private _apiservice:ApiserviceService){}
+
+    readData:any;
+
+    ngOnInit(): void{
+      this._apiservice.getApiData().subscribe((data)=>{
+        console.log("Helloworld", data);
+
+        this.readData = data.message;
+      });
+  }
 }
