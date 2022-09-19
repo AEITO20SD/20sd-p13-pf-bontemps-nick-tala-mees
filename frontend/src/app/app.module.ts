562f3c9ab023bc6796d0b059b3a254bb7e7c6d99
Meesie2109 <meesrootjes@icloud.com>
2022-09-19 11:09:22 +0200

    #37 naamgeving van de home component aangepast

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,17 +7,17 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ApiserviceService } from './apiservice.service';
 import { HeaderComponent } from './home/header/header.component';
-import { HomeComponent } from './home/home/home.component';
 import { UserInputComponent } from './users/shared/user-input/user-input.component';
 import { LoginComponent } from './users/login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent,
     UserInputComponent,
     LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
