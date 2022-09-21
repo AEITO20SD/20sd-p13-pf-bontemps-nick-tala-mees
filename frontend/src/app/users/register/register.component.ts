1baf9aa606e8677c4605056a917969e5805b41ec
Meesie2109 <meesrootjes@icloud.com>
2022-09-21 12:10:21 +0200

    #37 content aan de register page toegevoegd en wat styling

--- frontend/src/app/users/register/register.component.ts
+++ frontend/src/app/users/register/register.component.ts
@@ -9,6 +9,26 @@ export class RegisterComponent implements OnInit {
 
   constructor() { }
 
+  // varaibles for the first child input component
+  placeholder_email = 'BonTemps@example.com';
+  type_email = 'text';
+  name_email = 'email';
+
+  // varaibles for the second child input component
+  placeholder_firstname = 'firstname';
+  type_firstname = 'firstname';
+  name_firstname = 'firstname';
+
+  // varaibles for the second child input component
+  placeholder_secondname = 'secondname';
+  type_secondname = 'secondname';
+  name_secondname = 'secondname';
+
+  // varaibles for the second child input component
+  placeholder_pass = 'Password';
+  type_pass = 'password';
+  name_pass = 'password';
+
   ngOnInit(): void {
   }
 
