44653034b0e7ddb5e046369f9f694a695da5d30a
Meesie2109 <meesrootjes@icloud.com>
2022-09-23 01:15:25 +0200

    Html en css toegevoegd aan login-component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './users/pages/login/login.component';
 import { RegisterComponent } from './users/pages/register/register.component';
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
