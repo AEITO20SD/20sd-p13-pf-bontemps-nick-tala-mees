f16919a21121155bdb224fbee04dcead1b4b903c
Meesie2109 <meesrootjes@icloud.com>
2022-09-27 09:43:16 +0200

    #39 Reset page aangemaakt

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,15 +5,26 @@ import { HomeComponent } from './home/pages/home/home.component';
 import { LoginComponent } from './users/pages/login/login.component';
 import { RegisterComponent } from './users/pages/register/register.component';
 import { ResetSendComponent } from './users/pages/reset-send/reset-send.component';
+import { ResetComponent } from './users/pages/reset/reset.component';
 import { SendComponent } from './users/pages/send/send.component';
 import { VerifiedComponent } from './users/pages/verified/verified.component';
 
 const routes: Routes = [
+  // Landing page Route
   { path: '', component: HomeComponent },
+
+  // Register Routes
   { path: 'register', component: RegisterComponent },
+
+  // Emaul Verification Routes
   { path: 'register/send-verification-email', component: SendComponent },
   { path: 'register/verified', component: VerifiedComponent},
+
+  // Login routes
   { path: 'login', component: LoginComponent },
+
+  // Password reset routes
+  { path: 'login/reset-password', component: ResetComponent },
   { path: 'login/reset-password-send', component: ResetSendComponent },
 ];
 
