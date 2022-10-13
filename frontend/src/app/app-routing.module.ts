be5880529f160fdcdf366a3c8b8c0a8114cb7dec
Meesie2109 <meesrootjes@icloud.com>
2022-10-13 19:25:16 +0200

    login page volledig generic opgebouwd

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './modules/authentication/pages/login-page/login-page.component';
 import { LandingPageComponent } from './modules/landing/pages/landing-page/landing-page.component';
 
 const routes: Routes = [
   // Landing page Route
   { path: '', component: LandingPageComponent },
+
+
+  // login page Route
+  { path: 'login', component: LoginPageComponent }
 ];
 
 @NgModule({
